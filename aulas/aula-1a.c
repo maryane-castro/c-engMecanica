b8fd94073fd8c17be0f5063fcdf19cd47f0ee3d3
Maryane Castro <122397850+maryane-castro@users.noreply.github.com>
2024-09-30 21:46:38 -0300

    update

--- aulas/aula-1a.c
+++ aulas/aula-1a.c
@@ -1,31 +1,38 @@
 #include <stdio.h>  // biblioteca
 
+
+// declarar constantes
+#define NUMERO_DE_TENTATIVAS 5
+
 int main() {
     printf("\nBem vindo ao nosso jogo de adivinhação\n");
 
     int numero_secreto = 42;
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);        // recebe valor para a variavel
-    printf("Seu chute foi %d \n\n", chute);
+    for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
 
-    if (chute == numero_secreto){  // condicionais
-        printf("Parabéns você acertou!");
-    }
-    else{
-        if(chute > numero_secreto) {
-             printf("Seu chute foi maior que o número secreto\n");
+        int acertou = (chute == numero_secreto);
+
+        if (acertou) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+            break;
         }
+        else {
+            int maior = chute > numero_secreto;
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto\n");
 
-        if(chute < numero_secreto) {
-             printf("Seu chute foi menor que o número secreto\n");
+            }
         }
     }
-
 
     printf("\n\n");
-    
-    
-
 }
